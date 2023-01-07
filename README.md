3f9d979e27c2c6be06f777f8159d56f93a70414e
ChoCho66 <porkdie@gmail.com>
2023-01-07 12:20:21 +0800

    2023-01-07

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DL-Note
+
+
+
+
+
+---
+
+- https://quarto.org/docs/authoring/markdown-basics.html#page-breaks
+
+  - https://quarto.org/docs/extensions/shortcodes.html#raw-output
+
+  ```lua
+  contributes:
+    shortcodes:
+      - ../shorty.lua
+  ```
--- slide/revealjs_filter.lua
+++ slide/revealjs_filter.lua
@@ -1,3 +1,7 @@
+-- https://quarto.org/docs/extensions/lua.html
+-- https://quarto.org/docs/extensions/lua-api.html
+-- https://bookdown.org/yihui/rmarkdown-cookbook/lua-filters.html
+
 function Header(el)
   -- The header level can be accessed via the attribute 'level'
   -- of the element. See the Pandoc documentation later.
